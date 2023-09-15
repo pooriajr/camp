7ad10d8995286e3b5b4f06c4f55bb37b347a7a45
Pooria <pjrashidi@gmail.com>
2023-09-14 18:14:43 -0700

    WIP

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,8 +1,33 @@
 class CampersController < ApplicationController
+  before_action :set_camper, only: [:show, :edit, :update]
+
   def show
-    @camper = Camper.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    @camper.aka = nil if @camper.aka == ""
+    respond_to do |format|
+      if @camper.update(camper_params)
+        format.html { redirect_to camper_url(@camper), notice: "Camper was successfully updated." }
+        format.json { render :show, status: :ok, location: @camper }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @camper.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+
+  def camper_params
+    params.require(:camper).permit(:name, :aka, :project, :project_description, :project_url)
+  end
+
+  def set_camper
+    @camper = Camper.find(params[:id])
   end
 end
